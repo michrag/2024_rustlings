8f5389a772833bfbbe54ebd13eb838428cdbaef8
michrag <31441542+michrag@users.noreply.github.com>
2024-08-20 18:29:04 +0200

    quiz1, primitive_types and vecs done

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,11 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    if apples <= 40 {apples * 2}
+    else {apples}
+}
+
 fn main() {
     // You can optionally experiment here.
 }
